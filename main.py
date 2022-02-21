4c87ba23ab8297ba9b7d60e02e55cda0a2dce54e
vNziie-- <80853540+Nziie3@users.noreply.github.com>
2022-02-20 16:25:17 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# MAIN BOT FILE
+
+import discord
+from discord.ext import commands
+
+PREFIX = "!" # you can change this to what you want (e.g. '>'; '-'; '?')
+
+client = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
+# Now, there are 3 things you can do to make your bot:
+# discord.Bot > slash commands, other UI stuff (buttons, selects, context commands/apps, modals), events
+# discord.Client > only events (@client.event, @commands.Cog.listener)
+# commands.Bot > everything that a bot can have (everything mentioned above)
+
+@client.event
+async def on_ready():
+  print(f'{client.user} is ready!')
+
+
+TOKEN = "put your token here" # put your bot's token in the string, you can get your bot's token from the Developer Portal
+client.run(TOKEN)
