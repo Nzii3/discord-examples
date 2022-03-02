6d16b5ed654a049e08267d9bebd38beb6f299123
Nziie3 <nziie124@gmail.com>
2022-03-02 14:56:30 -0800

    Update modal.py

--- beta/modal.py
+++ beta/modal.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 class MyModal(discord.ui.Modal):
   def __init__(self):
     super.__init__("My app title") # setting the app title/name
-    self.add_item(discord.ui.InputText(label="My app question", placeholder="My placeholder", min_length=1, max_length=100)
+    self.add_item(discord.ui.InputText(label="My app question", placeholder="My placeholder", min_length=1, max_length=100))
     # Optional arguments:
     # 'required' (default = True)
     # 'style' discord.InputTextStyle (default = short)
@@ -16,6 +16,11 @@ class MyModal(discord.ui.Modal):
     # 'value' this is the test that is already in the field (default = None)
     # 'min_length' inherits from 'style' kwarg
     # 'max_length' inherits from 'style' kwarg (default = 4000 - I think)
+    async def mycallback(self, interaction: discord.Interaction):
+      embed = discord.Embed(color=discord.Colour.blurple(), title="Your Application Results")
+      for item in self.children:
+        embed.add_field(name=item.label, value=item.value)
+      await interaction.response.send_message(embed=embed, ephemeral=True) # sending all of our answers in an embed that's hidden
 
 
 ### TO DO:
@@ -23,3 +28,5 @@ class MyModal(discord.ui.Modal):
 # Slash command that invokes the modal
 # Select that invokes the modal
 # Context command that invokes the modal
+
+
